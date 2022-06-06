e62863f8820507a9b30859195ff292f18ffa044b
q542718337 <1093422166@qq.com>
2022-06-06 19:30:44 +0800

    Add files via upload

---
+++ src/main/java/me/yunoe/yunoeredislib/YunoeRedisLib.java
@@ -0,0 +1,25 @@
+package me.yunoe.yunoeredislib;
+/*
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class YunoeRedisLib extends JavaPlugin {
+    public static YunoeRedisLib plugin;
+
+    @Override
+    public void onEnable() {
+        plugin = this;
+
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}*/
+
+public final class YunoeRedisLib {
+    public YunoeRedisLib() {
+        System.out.println("YunoeRedisLib已加载！");
+    }
+}
